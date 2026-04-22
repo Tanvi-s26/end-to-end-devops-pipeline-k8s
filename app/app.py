b9ce345473c88c74038be223820ccc139c4812fc
Tanvi <tanviijanday@gmail.com>
2026-04-22 20:23:05 +0530

    Fixed CI/CD pipeline (removed invalid k8s deploy step)

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Tanvi, DevOps Project - Version 1 🚀"
+    return "Hello from Tanvi, DevOps Project - Version 2 🚀"
 
 @app.route('/health')
 def health():
